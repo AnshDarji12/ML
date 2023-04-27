29f0a3252265039400ddfbad1295148ebdaf89d6
Ansh Darji <anshdarji96@gmail.com>
2023-04-27 22:01:20 +0530

    first commit

---
+++ application.py
@@ -0,0 +1,49 @@
+from flask import Flask, request,app,render_template
+import numpy as np
+import pandas as pd
+import pickle
+from flask import Response
+
+
+application = Flask(__name__)
+app=application
+scaler=pickle.load(open('Model/scaler (1).pkl','rb'))
+model = pickle.load(open('Model/Log.pkl','rb'))
+
+
+#Route for single data point prediction
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    result=" "
+
+    if request.method =='POST':
+        Pregnancies =int(request.form.get('Pregnancies'))
+        Glucose =float(request.form.get('Glucose'))
+        BloodPressure =float(request.form.get('BloodPressure'))
+        SkinThickness =float(request.form.get('SkinThickness'))
+        Insulin =float(request.form.get('Insulin'))
+        BMI =float(request.form.get('BMI'))
+        DiabetesPedigreeFunction =float(request.form.get('DiabetesPedigreeFunction'))
+        Age =float(request.form.get('Age'))
+
+
+
+        new_data = scaler.transform([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
+        predict = model.predict(new_data)
+
+        if predict[0] == 1:
+            result = 'Diabetes'
+
+        else:
+            result = 'Non-Diabetes'
+
+        return render_template('predictdata.html',result=result)
+
+    else:
+        return render_template('home.html')
+
+
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
